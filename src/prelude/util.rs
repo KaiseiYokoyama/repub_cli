687ef78cb784c7882ea995ce9d919ec4a8a76e4e
Unknown <crome110mine@gmail.com>
2019-09-12 23:20:34 +0900

    impl 警告表示

--- src/prelude/util.rs
+++ src/prelude/util.rs
@@ -27,4 +27,31 @@ pub mod message {
             }
         }
     }
+
+    pub mod warning {
+        use std::fmt::{Display, Formatter};
+        use colored::Colorize;
+
+        pub struct RepubWarning(pub String);
+
+        impl AsRef<str> for RepubWarning {
+            fn as_ref(&self) -> &str {
+                let RepubWarning(s) = &self;
+                s.as_str()
+            }
+        }
+
+        impl From<&str> for RepubWarning {
+            fn from(s: &str) -> Self {
+                Self(s.to_string())
+            }
+        }
+
+        impl Display for RepubWarning {
+            fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+                use colored::*;
+                write!(f, "{} {}","[Warning]".yellow().bold(), &self)
+            }
+        }
+    }
 }
