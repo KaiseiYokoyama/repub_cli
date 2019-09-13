a1e8ca49b4727576f6df1d15a6395bd5d64fa433
Unknown <crome110mine@gmail.com>
2019-09-13 15:02:09 +0900

    impl 簡単なテストコード

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,12 @@ use crate::{prelude::*, load::*, data::*};
 
 fn main() -> RepubResult<()> {
     let app = crate::app::app();
-    let input = Input::try_from(app.get_matches())?;
 
-    println!("{:?}", input);
+    let input = Input::try_from(app.get_matches())?;
+    println!("{:?}", &input);
+
+    let data = Data::from(input);
+    println!("{:?}", &data);
+
     Ok(())
 }
