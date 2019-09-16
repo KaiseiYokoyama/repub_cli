84fcbc8044b0cd3d8571d2bb9e5a707c7eb5710c
Unknown <crome110mine@gmail.com>
2019-09-16 14:35:44 +0900

    fix xhtml presentation

--- src/compose.rs
+++ src/compose.rs
@@ -171,7 +171,15 @@ impl Composer {
             serialize(&mut bytes, &dom.document.children.borrow()[0], SerializeOpts::default()).unwrap();
             let xhtml = String::from_utf8(bytes).unwrap();
             // domをhtmlに変換しているので、xhtmlとは文法の合わない箇所がある
-            xhtml.replace("<br>","<br />")
+            // <br> -> <br />
+            let xhtml = xhtml.replace("<br>", "<br />");
+            // <img src="image.png" alt="image"> ->  <img src="image.png" alt="image" />
+            let peaces: Vec<&str> = xhtml.split('<').collect();
+            peaces.into_iter().map(|s| {
+                        if s.starts_with("img") {
+                            s.replacen(">", " />", 1)
+                        } else { s.to_string() }
+            }).collect::<Vec<String>>().join("<")
         }
 
         for file in &self.data.files.content_files {
